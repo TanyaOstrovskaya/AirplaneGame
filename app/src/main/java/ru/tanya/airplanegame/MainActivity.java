d9005b1b33ef43c70ab783512d34c058cb6f2898
Tatyana Ostrovskaya <ostrtanja@gmail.com>
2017-02-07 20:48:22 +0300

    modified image object classes, modified CanvasView

--- app/src/main/java/ru/tanya/airplanegame/MainActivity.java
+++ app/src/main/java/ru/tanya/airplanegame/MainActivity.java
@@ -1,7 +1,12 @@
 package ru.tanya.airplanegame;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,9 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-//        ImageView iv = (ImageView) findViewById(R.id.ImageView);
-//        iv.setImageResource(R.drawable.plane);
+        CanvasView canvasView = new CanvasView(this);
+        setContentView(canvasView);
     }
 }
