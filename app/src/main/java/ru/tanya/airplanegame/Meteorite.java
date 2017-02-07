38e28e766a25ec991532f3a420b08537fe9d9c7a
Tatyana Ostrovskaya <ostr-tanja@gmail.com>
2017-02-07 16:42:52 +0300

    added image object classes

---
+++ app/src/main/java/ru/tanya/airplanegame/Meteorite.java
@@ -0,0 +1,10 @@
+package ru.tanya.airplanegame;
+
+import android.graphics.Bitmap;
+
+public class Meteorite extends SimpleImageObject {
+    
+    public Meteorite(int x, int y, Bitmap bitmap) {
+        super(x, y, bitmap);
+    }
+}
