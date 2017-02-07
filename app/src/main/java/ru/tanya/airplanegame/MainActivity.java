c2bfc650836b61a84c7143bae819c2185b2b73cb
Tatyana Ostrovskaya <ostrtanja@gmail.com>
2017-02-07 15:33:15 +0300

    created main classes, added resouces

---
+++ app/src/main/java/ru/tanya/airplanegame/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.tanya.airplanegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ImageView iv = (ImageView) findViewById(R.id.ImageView);
+//        iv.setImageResource(R.drawable.plane);
+    }
+}
