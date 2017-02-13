deffeb139c2a12c06a50e0c582000d46f22606f5
Tatyana Ostrovskaya <ostrtanja@gmail.com>
2017-02-13 17:30:07 +0300

    fixed airplane movement

--- app/src/main/java/ru/tanya/airplanegame/Airplane.java
+++ app/src/main/java/ru/tanya/airplanegame/Airplane.java
@@ -4,7 +4,7 @@ import android.graphics.Bitmap;
 
 public class Airplane extends SimpleImageObject{
 
-    public static final int SPEED_COEFFICIENT = 1; //speed on OX
+    public static final double SPEED_COEFFICIENT = 0.5; //speed on OX
     private int angle;
 
     public Airplane(int x, int y, Bitmap bitmap) {
@@ -23,11 +23,34 @@ public class Airplane extends SimpleImageObject{
     @Override
     public void updateCoordinates() {
         if (checkBounds()) {
-            x += SPEED_COEFFICIENT*angle;
+            x += SPEED_COEFFICIENT * angle;
+        } else {
+            // if plane is near RIGHT display border
+            if (x > GameManager.getWidth()/2) {
+                // if user tryes to move plain to the LEFT side
+                // then update coordinates, else do nothing
+                if (angle <= 0) {
+                    x += SPEED_COEFFICIENT * angle;
+                }
+            } else {
+                // if plane is near LEFT display border
+
+                // if user tryes to move plain to the RIGHT side
+                // then update coordinates, else do nothing
+                if (angle > 0) {
+                    x += SPEED_COEFFICIENT * angle;
+                }
+            }
         }
     }
 
     public void setAngle(int angle) {
-        this.angle = angle;
+        // modifies angle from (0 .. 360) to (-90 .. 90)
+        if (angle < 90) {
+            this.angle = angle;
+        } else {
+            this.angle = angle - 360;
+        }
+
     }
 }
